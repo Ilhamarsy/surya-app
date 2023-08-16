7c3455a34696be2e0176a2c1f76d1b127817e4d4
ilham arsy <ilhamarsy16@gmail.com>
2023-08-16 21:40:16 +0800

    update automation

--- start.sh
+++ start.sh
@@ -10,18 +10,18 @@ pwd
 echo "list folder"
 ls -l
 
-echo "add env"
-echo DB_SOURCE=$DB_SOURCE >> .env
-echo DB_DRIVER=$DB_DRIVER >> .env
-echo DB_SOURCE=$SERVER_ADDRESS >> .env
-echo DB_DRIVER=$ACCESS_TOKEN_PRIVATE_KEY >> .env
-echo DB_SOURCE=$ACCESS_TOKEN_PUBLIC_KEY >> .env
-echo DB_DRIVER=$ACCESS_TOKEN_EXPIRED_IN >> .env
-echo DB_SOURCE=$ACCESS_TOKEN_MAXAGE >> .env
-echo DB_DRIVER=$REFRESH_TOKEN_PRIVATE_KEY >> .env
-echo DB_DRIVER=$REFRESH_TOKEN_PUBLIC_KEY >> .env
-echo DB_DRIVER=$REFRESH_TOKEN_EXPIRED_IN >> .env
-echo DB_DRIVER=$REFRESH_TOKEN_MAXAGE >> .env
+# echo "add env"
+# echo DB_SOURCE=$DB_SOURCE >> .env
+# echo DB_DRIVER=$DB_DRIVER >> .env
+# echo DB_SOURCE=$SERVER_ADDRESS >> .env
+# echo DB_DRIVER=$ACCESS_TOKEN_PRIVATE_KEY >> .env
+# echo DB_SOURCE=$ACCESS_TOKEN_PUBLIC_KEY >> .env
+# echo DB_DRIVER=$ACCESS_TOKEN_EXPIRED_IN >> .env
+# echo DB_SOURCE=$ACCESS_TOKEN_MAXAGE >> .env
+# echo DB_DRIVER=$REFRESH_TOKEN_PRIVATE_KEY >> .env
+# echo DB_DRIVER=$REFRESH_TOKEN_PUBLIC_KEY >> .env
+# echo DB_DRIVER=$REFRESH_TOKEN_EXPIRED_IN >> .env
+# echo DB_DRIVER=$REFRESH_TOKEN_MAXAGE >> .env
 
 echo "start the app"
 exec "$@"
